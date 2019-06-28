1594f7cde66b2602cfe77ed4ed0452a3f38da306
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-06-28 18:11:55 -0400

    created all files and folders and began builing html page wireframe according to bootsrap grid, as well as started making characters object with characters inside

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+// characters: darth, princess, yoda, emperor
+//Each character has 3 attributes: 
+//`Health Points`, `Attack Power` and `Counter Attack Power`.
+
+$(document).ready(function() {
+var characters = {
+
+    "Darth Vadar" :  {
+        health_points: 120,
+        attack_power: 6,
+        counter_attack_power: 40,
+        image: "../images",
+        name: "Darth Vadar"
+    },
+
+    "Emperor" :  {
+        health_points: 120,
+        attack_power: 6,
+        counter_attack_power: 40,
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor.jpg",
+        name: "Darth Vadar"
+    },
+
+    "Yoda" :  {
+        health_points: 120,
+        attack_power: 6,
+        counter_attack_power: 40,
+        image: "../images/yoda.jpg",
+        name: "Darth Vadar"
+    },
+
+    "Princess" :  {
+        health_points: 120,
+        attack_power: 6,
+        counter_attack_power: 40,
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor.jpg",
+        name: "Darth Vadar"
+    },
+
+};
+
+var playerImg = $("<img alt='image' class='character-image'>").attr("src", characters["Darth Vadar"].image);
+$("#player").append(playerImg)
+
+
+});
