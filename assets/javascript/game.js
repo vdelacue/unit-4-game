aba111dbc9f16ba6ae313dc3d117bcb690dc5a40
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-06-28 23:58:46 -0400

    started game logic by creating objects of characters then global variables needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,34 +6,34 @@ $(document).ready(function() {
 var characters = {
 
     "Darth Vadar" :  {
-        health_points: 120,
-        attack_power: 6,
-        counter_attack_power: 40,
+        health_points: 100,
+        attack_power: 5,
+        counter_attack_power: 5,
         image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/darth2.jpg",
         name: "Darth Vadar"
     },
 
     "Emperor" :  {
         health_points: 120,
-        attack_power: 6,
-        counter_attack_power: 40,
-        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor.jpg",
+        attack_power: 8,
+        counter_attack_power: 8,
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor2.jpg",
         name: "Darth Vadar"
     },
 
     "Yoda" :  {
-        health_points: 120,
-        attack_power: 6,
-        counter_attack_power: 40,
-        image: "../images/yoda.jpg",
+        health_points: 180,
+        attack_power: 25,
+        counter_attack_power: 25,
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/yoda2.jpg",
         name: "Darth Vadar"
     },
 
     "Princess" :  {
-        health_points: 120,
-        attack_power: 6,
-        counter_attack_power: 40,
-        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor.jpg",
+        health_points: 150,
+        attack_power: 20,
+        counter_attack_power: 20,
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/princess2.jpg",
         name: "Darth Vadar"
     },
 
