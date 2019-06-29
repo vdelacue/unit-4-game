57deab1bd02b6a1c61ed47edf31a0ab2ae9f4ac0
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-06-28 23:07:03 -0400

    added 6 rows and psuedo coded content for each, added styling to background and added images to starting game play, creating appropriate classes and id to target specific rows and divs during game play

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-// characters: darth, princess, yoda, emperor
-//Each character has 3 attributes: 
-//`Health Points`, `Attack Power` and `Counter Attack Power`.
+// create an object of characters: with each character as another object :
+//4 characters: darth, princess, yoda, emperor
+//Each character has 3 attributes: 'Health Points`, `Attack Power` and `Counter Attack Power`.
 
 $(document).ready(function() {
 var characters = {
@@ -9,7 +9,7 @@ var characters = {
         health_points: 120,
         attack_power: 6,
         counter_attack_power: 40,
-        image: "../images",
+        image: "/Users/vanessa/Rutgers/HOMEWORK ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/darth2.jpg",
         name: "Darth Vadar"
     },
 
@@ -38,6 +38,7 @@ var characters = {
     },
 
 };
+
 
 var playerImg = $("<img alt='image' class='character-image'>").attr("src", characters["Darth Vadar"].image);
 $("#player").append(playerImg)
