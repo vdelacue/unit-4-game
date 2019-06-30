79a04eecf51e4bcb0acecd92c0f4fa2425cbbbb2
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-06-30 14:31:49 -0400

    created first jquery on click function for first player darth and ran initializer functions, added styling to game play for first click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,14 @@
 //Each character has 3 attributes: 'Health Points`, `Attack Power` and `Counter Attack Power`.
 
 $(document).ready(function () {
-    var players = {
 
+    var players = {
         "Darth Vadar": {
             name: "Vadar",
             health_points: 100,
             attack_power: 5,
             counter_attack_power: 5,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/darth2.jpg"
-
         },
 
         "Emperor": {
@@ -20,7 +19,6 @@ $(document).ready(function () {
             attack_power: 8,
             counter_attack_power: 8,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor2.jpg"
-
         },
 
         "Yoda": {
@@ -29,7 +27,6 @@ $(document).ready(function () {
             attack_power: 25,
             counter_attack_power: 25,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/yoda2.jpg"
-
         },
 
         "Princess": {
@@ -38,8 +35,7 @@ $(document).ready(function () {
             attack_power: 20,
             counter_attack_power: 20,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/princess2.jpg"
-
-        },
+        }
 
     };
     // var playerImg = $("<img alt='image' class='character-image'>").attr("src", characters["Darth Vadar"].image);
@@ -74,27 +70,47 @@ $(document).ready(function () {
         player.health_points = playerSelected.health_points;
         player.attack_power = playerSelected.attack_power;
         player.counter_attack_power = playerSelected.counter_attack_power;
-    }
+        
+    };
     // function will set the defender selected values
     function initializeDefender(defenderSelected) {
         defender.name = defender.name;
         defender.health_points = defender.health_points;
         defender.attack_power = defender.attack_power;
         defender.counter_attack_power = defender.counter_attack_power;
-    }
+    };
     // function will take players not selected and move them into defender selection section 
     // and remove the selected defender into a non displayed class
-    function initializeDefenderRow {
-        $(".playerSelection").removeClass( "playerSelection" ).addClass("defenderSelection");
+    function initializeDefenderRow() {
+        $(".playerSelection").removeClass("playerSelection").addClass("defenderSelection");
         $("#defender").appendTo(".defendersFought");
-    }
+    };
 
-    function reset {
+    function reset() {
         var playerSelected = false;
         var defenderSelected = false;
         player = {};
-        defender {};
+        defender = {};
         var defendersDefeated = 0;
         var gameOver = false;
     }
+
+// make a jQuery on click function for each of 4 players that determines if they are selected where all other players are moved
+$("#darth").on("click", function(){
+    initializePlayer(players["Darth Vadar"]);
+    $("#darth").appendTo("#player");
+});
+
+$("#emperor").on("click", function(){
+    initializePlayer(players.Emperor);
+});
+
+$("#yoda").on("click", function(){
+    initializePlayer(players.Yoda);
+});
+
+$("#princess").on("click", function(){
+    initializePlayer(players.Princess);
+});
+
 });
