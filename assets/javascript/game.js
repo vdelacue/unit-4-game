b672b8e777007f5a6964e908788c203962d77d51
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-07-02 21:00:11 -0400

    changed health points and attack power to increase game difficulty resolved lighsaber sound issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
         "Darth Vadar": {
             name: "Darth",
             health_points: 100,
-            attack_power: 20,
-            counter_attack_power: 20,
+            attack_power: 5,
+            counter_attack_power: 5,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/darth2.jpg",
             defeated: false
         },
@@ -17,8 +17,8 @@ $(document).ready(function () {
         "Emperor": {
             name: "Emperor",
             health_points: 120,
-            attack_power: 10,
-            counter_attack_power: 10,
+            attack_power: 8,
+            counter_attack_power: 8,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/emperor2.jpg",
             defeated: false
         },
@@ -26,8 +26,8 @@ $(document).ready(function () {
         "Yoda": {
             name: "Yoda",
             health_points: 180,
-            attack_power: 5,
-            counter_attack_power: 5,
+            attack_power: 25,
+            counter_attack_power: 25,
             image: "/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/images/yoda2.jpg",
             defeated: false
         },
@@ -66,7 +66,7 @@ $(document).ready(function () {
 
     //---------------sounds --------------//
     // attack sound
-    var attMP3 = new Audio("../sounds/coolsaber.mp3");
+    var attMP3 = new Audio("/Users/vanessa/Rutgers/HOMEWORK_ASSIGNMENTS/hw-4-jquery/unit-4-game/assets/sounds/coolsaber.mp3");
     attMP3.volume = 0.2;
 
     // --------- code was done with mentor to catch error for unfullfilled promise with audio play
